70ec4cee0856732500907fbcd82c622e1bd4f769
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-06-10 16:59:45 -0300

    Teste Jogos

--- cafetaverna/index.php
+++ cafetaverna/index.php
@@ -5,9 +5,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Café Taverna</title>
-    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/all.css">
 </head>
 <body>
-    
+
+<header>
+    <a href="index.php" id="logo">
+        Café Taverna
+    </a>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section id="banner">
+        <h3>🏰Jogos de RPG🏰</h3>
+        <div id="efeito">
+            <div id="personagem">🐱‍👤</div>
+        </div>
+    </section>
+
+    <section id="sobreJogos">
+        <h3>Jogos Disponíveis</h3>
+        <div id="todosJogos">
+            <div class="jogo">
+                    <img src="images/The Witcher.png" alt="">
+                    <p>The Witcher</p>
+            </div>
+            <div class="jogo">
+                    <img src="images/NBA 2K.png" alt="">
+                    <p>NBA 2K25</p>
+            </div>
+            <div class="jogo">
+                    <img src="images/valorant.png" alt="">
+                    <p>Valorant</p>
+            </div>
+        </div>
+    </section>
+</main>
+<footer></footer>
+
+<script src="scripts/script.js"></script>
 </body>
 </html>
